fb6d2c5879a409606056be20171ca3d485f47fe4
Yury Lapitski <yury.connect@gmail.com>
2025-07-19 10:23:10 +0300

    auto_save: 2025-07-19 10:23:10 _from Yury_Home-PC 3 changed: A src/main/java/com/example/demo/controller/api/ApiController.java src/main/java/com/example/demo/controller/mvc/MvcController.java, M src/main/resources/application.properties

---
+++ src/main/java/com/example/demo/controller/api/ApiController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/hello")
+public class ApiController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello from REST API!";
+    }
+}
---
+++ src/main/java/com/example/demo/controller/mvc/MvcController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MvcController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to Thymeleaf!");
+        return "home";
+    }
+}
